2de0e170e3aecdd8da245a812d68a852d0e39106
ohnorobo <sar.laplante@gmail.com>
2015-02-01 18:49:32 -0500

    working twitter firehose and counting syllables

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 
+import os, time, sys
 import tweepy
-import os
 import postgresql
 from pprint import pprint
 from keys import *
@@ -12,6 +12,11 @@ def log_error(msg):
 
 def is_ascii(s):
   return all(ord(c) < 128 for c in s)
+
+def contains_digit(string):
+  return any(char.isdigit() for char in string)
+
+
 
 class StreamWatcherListener(tweepy.StreamListener):
   def on_status(self, status):
@@ -31,11 +36,119 @@ class StreamWatcherListener(tweepy.StreamListener):
 
 
 class Database():
+
+  def __init__(self):
+    self.mr = MeterReader()
+
   def potential_add(self, tweet):
 
-    if (is_ascii(tweet.text) and tweet.lang == "en"):
-      pprint(tweet.text)
-      pprint(tweet.id)
+    if self.validate(tweet):
+      if (self.mr.valid_meter(tweet.text)):
+        pprint(tweet.text)
+        pprint(tweet.id)
+
+  # meets some strict list of criteria for inclusion
+  # ascii only
+  # english only
+  # no retweets
+  # no included links
+  def validate(self, tweet):
+    return is_ascii(tweet.text) and \
+           tweet.lang == "en"
+
+
+
+class MeterReader():
+
+  def __init__(self):
+    self.dic = CMUDict()
+
+  def valid_meter(self, text):
+    text = text.split(' ')
+
+    try:
+      num = self.num_sylls(text)
+      print(num)
+    except KeyError:
+      return False
+
+    if 8 <= num <= 10:
+      if self.matchlong(text):
+        return "long"
+
+    if 5 <= num <= 7:
+      if self.matchshort(text):
+        return "short"
+
+    return False
+
+  # return the number of sylls in a text
+  def num_sylls(self, text):
+    return sum(map(self.dic.sylls, text))
+
+  # match the pattern 
+  # ˘ ˘ / ˘ ˘ / ˘ ˘ /
+  # where single-syll words match either way
+  def matchlong(self, text):
+    return True
+
+  # match the pattern 
+  # ˘ ˘ / ˘ ˘ /
+  # where single sylls count either way
+  def matchshort(self, text):
+    return True
+
+
+class CMUDict():
+
+  def __init__(self):
+
+    f = open("./cmudict/cmudict-0.7b")
+
+    d = {}
+    for line in f:
+      word, syllbit = line.split("  ") # two spaces
+      sylls = syllbit.strip().split(' ') # one space
+      d[word]=(self.parse_stresses(sylls),
+               self.parse_rhyme(sylls),
+               self.parse_num_sylls(sylls))
+
+      #pprint((line, d))
+    self.dic = d
+
+  # returns the number of sylls in a word
+  def parse_num_sylls(self, sylls):
+    return len(list(filter(contains_digit, sylls)))
+
+  # returns a list of 0s 1s and 2s, 
+  # corrosponding to the stresses in the given syllables
+  def parse_stresses(self, sylls):
+    stresses = []
+    for syll in sylls:
+      if '0' in syll:
+        stresses.append(0)
+      if '1' in syll:
+        stresses.append(1)
+      if '2' in syll:
+        stresses.append(2)
+
+    return stresses
+
+  # returns a string which represents the rhyme for the word
+  def parse_rhyme(self, sylls):
+    return sylls[-1]
+    #TODO how many strings are best?
+
+  def sylls(self, word):
+    return self.dic[word.upper()][2]
+
+  def stresses(self, word):
+    return self.dic[word.upper()][0]
+
+  def rhyme(self, word):
+    return self.dic[word.upper()][1]
+
+
 
 
 #main
@@ -45,6 +158,11 @@ db = Database()
 
 listener = StreamWatcherListener()
 stream = tweepy.Stream(auth, listener)
+
+# stream.filter(languages=["en"])
+# this doesn't seem to work at the moment
+# https://github.com/tweepy/tweepy/issues/291
+
 stream.sample()
 
 

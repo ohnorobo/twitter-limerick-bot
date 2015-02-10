ddbe71c12d8f6ad90058a41edded8deccb574bd2
ohnorobo <sar.laplante@gmail.com>
2015-02-09 21:43:21 -0500

    adding simple things to db

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ import postgresql
 from pprint import pprint
 from keys import *
 
+
+VOWELS = ['A', 'E', 'I', 'O', 'U'] # vowels parts in the CMU phone schema
+
 def log_error(msg):
   timestamp = time.strftime('%Y%m%d:%H%M:%S')
   sys.stderr.write("%s: %s\n" % (timestamp,msg))
@@ -43,14 +46,41 @@ class StreamWatcherListener(tweepy.StreamListener):
 class Database():
 
   def __init__(self):
+    db = postgresql.open(user='slaplante', password='',
+                         database='limerickdb',
+                         host='localhost', port=5432)
+    self.add = db.prepare("INSERT INTO limericks VALUES ($1, $2, $3, $4)")
+
+
     self.mr = MeterReader()
+
 
   def potential_add(self, tweet):
 
     if self.validate(tweet):
-      if (self.mr.valid_meter(tweet.text)):
+      # Valid is one of "long" "short" False
+      valid = self.mr.valid_meter(tweet.text)
+
+      if (valid):
         pprint(tweet.text)
         pprint(tweet.id)
+        pprint(valid)
+        rhyme = self.mr.rhyme(tweet.text)
+
+        self.add(tweet.id, tweet.text, rhyme,
+                 self.long_not_short(valid))
+
+  # long or short is stored in the db as a boolean
+  # T = long
+  # F = short
+  def long_not_short(self, lors):
+    if lors == "long":
+      return True
+    elif lors == "short":
+      return False
+    else:
+      print("AAAHHH")
+
 
   # meets some strict list of criteria for inclusion
   # ascii only
@@ -72,6 +102,28 @@ class MeterReader():
   def __init__(self):
     self.dic = CMUDict()
 
+  def rhyme(self, text):
+    text = text.split(' ')
+    # get last 2 words for the rhyme
+    final_sylls = flatten([self.dic.sound_sylls(word) for word in text[-2:]])
+
+    for i, syll in enumerate(reversed(final_sylls)):
+      if any(vowel in syll for vowel in VOWELS):
+        break
+
+    # TODO, this includes the consonant before the last vowel
+    # is that really what we want?
+    # maybe it should just be core+ryme
+    # or head+core if there's no ryme
+    # tho -> DH-OW
+    # family -> L-IY
+    rhyme_sylls = final_sylls[-1*(i+2):]
+    st = "-".join(rhyme_sylls)
+    print(st)
+    return st;
+
+
+
   def valid_meter(self, text):
     text = text.split(' ')
 
@@ -92,7 +144,7 @@ class MeterReader():
 
   # return the number of sylls in a text
   def num_sylls(self, text):
-    return sum(map(self.dic.sylls, text))
+    return sum(map(self.dic.num_sylls, text))
 
   # match the pattern 
   # + ˘ / ˘ ˘ / ˘ ˘ / +
@@ -196,6 +248,9 @@ STOPWORDS = ["a", "the", "for", "am", "an", "are", "as", "at", "be", "but",
 
 class CMUDict():
 
+  # dic is a dicitonary of 
+  # { words : [stresses, syllable_sounds, number_of_sylls] }
+
   def __init__(self):
 
     f = open("./cmudict/cmudict-0.7b")
@@ -205,13 +260,16 @@ class CMUDict():
       word, syllbit = line.split("  ") # two spaces
       sylls = syllbit.strip().split(' ') # one space
       d[word]=(self.parse_stresses(sylls),
-               self.parse_rhyme(sylls),
+               self.parse_syll_sounds(sylls),
                self.parse_num_sylls(sylls))
     self.dic = d
 
   # returns the number of sylls in a word
   def parse_num_sylls(self, sylls):
     return len(list(filter(contains_digit, sylls)))
+
+  def parse_syll_sounds(self, sylls):
+    return [self.remove_num(syll) for syll in sylls]
 
   # returns a list of 0s 1s and 2s, 
   # corrosponding to the stresses in the given syllables
@@ -228,18 +286,29 @@ class CMUDict():
     return stresses
 
   # returns a string which represents the rhyme for the word
-  def parse_rhyme(self, sylls):
-    return sylls[-1]
-    #TODO how many strings are best?
+  #def parse_rhyme(self, sylls):
+  #  return sylls[-1]
+  #  #TODO how many strings are best?
 
-  def sylls(self, word):
+  def num_sylls(self, word):
     return self.dic[word.upper()][2]
+
+  # like sylls, but removes stress #s in syllables
+  def sound_sylls(self, word):
+    #print((word, self.dic[word.upper()]))
+    return [self.remove_num(syll) for syll in self.dic[word.upper()][1]]
+
+  def remove_num(self, syll):
+    if '0' in syll or '1' in syll or '2' in syll:
+      return syll[:-1]
+    else:
+      return syll
 
   def stresses(self, word):
     return self.dic[word.upper()][0]
 
-  def rhyme(self, word):
-    return self.dic[word.upper()][1]
+  #def rhyme(self, word):
+  #  return self.dic[word.upper()][1]
 
   # returns [True] if it's a single syll word
   # returns [False False ...] for any multi-syll word
